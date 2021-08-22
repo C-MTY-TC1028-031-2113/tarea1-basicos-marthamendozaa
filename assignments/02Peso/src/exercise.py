5b221af31859eab9c6b8397e977921d6a831e613
Martha <a01284654@itesm.mx>
2021-08-22 18:39:59 -0500

    220821

--- assignments/02Peso/src/exercise.py
+++ assignments/02Peso/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    pi = float(input("Dame el peso inicial: "))
+    pf = float(input("Dame el peso final: "))
+    mes = int(input("Dame la cantidad de meses: "))
+    bajar = float((pi-pf)/mes)
+    print("Lo que debes bajar por mes es: " + str(bajar))
+
 
 
 
